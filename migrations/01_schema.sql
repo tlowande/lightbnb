428144d8a90756f79f59d40b24c8e4f54c3169e2
Tamires <tamires.lowande@gmail.com>
2019-10-16 20:12:24 +0000

    added new files from github

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS properties CASCADE;
 DROP TABLE IF EXISTS reservations CASCADE;
 DROP TABLE IF EXISTS property_reviews CASCADE;
+DROP TABLE IF EXISTS rates CASCADE;
+DROP TABLE IF EXISTS guest_reviews CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -64,4 +66,12 @@ CREATE TABLE guest_reviews (
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
+);
+
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  cost_per_night INTEGER NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
 );
